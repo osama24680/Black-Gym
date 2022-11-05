d95128bdf82c19c8ec71d01cdc159878c831a1e4
Osama <osamamegahed2468@gmail.com>
2022-11-05 16:55:16 +0200

    fv

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': '15b423e5c1msh4ed27c6139bfa94p1ee7d4jsnfda6bb6c2fa6',
+    'X-RapidAPI-Key': 'da50e867dfmsh62c7b3341bd1710p101c9fjsna24de86db22e',
   },
 };
 
